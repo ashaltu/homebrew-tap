9b84ea4d361c05e80917e45d7f660fdaf0cde1f4
ashaltu <abdul.shaltu17@gmail.com>
2025-11-22 10:52:33 -0800

    Fix style in qvm.rb

--- Formula/qvm.rb
+++ Formula/qvm.rb
@@ -1,5 +1,5 @@
 class Qvm < Formula
-  desc "A C++/FFmpeg tool that generates high-quality Quran verse videos."
+  desc "C++/FFmpeg tool that generates high-quality Quran verse videos"
   homepage "https://github.com/ashaltu/quran-video-maker-ffmpeg"
   url "https://github.com/ashaltu/quran-video-maker-ffmpeg/releases/download/v0.1.0/qvm-v0.1.0.tar.gz"
   sha256 "392adf899b62cdbd7087dbd268992e1bacd8fc95b211c56eaeb848026da0e90e"
@@ -7,12 +7,12 @@ class Qvm < Formula
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "cpr"
+  depends_on "cxxopts"
   depends_on "ffmpeg"
   depends_on "freetype"
   depends_on "harfbuzz"
-  depends_on "cpr"
   depends_on "nlohmann-json"
-  depends_on "cxxopts"
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
